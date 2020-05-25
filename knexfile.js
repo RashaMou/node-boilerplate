4f0575e319bd0b338cbecdebf95e54dec4591fd8
Rasha Moumneh <rasha@fastmail.com>
2020-05-25 15:27:58 -0400

    Setup knex config

---
+++ knexfile.js
@@ -0,0 +1,51 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {
+      host: "127.0.0.1",
+      user: "[db_username]",
+      password: "[db_password]",
+      database: "[db_name]",
+      charset: "utf8",
+    },
+    migrations: {
+      directory: __dirname + "/database/migrations",
+    },
+    seeds: {
+      directory: __dirname + "/databse/seeds",
+    },
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
